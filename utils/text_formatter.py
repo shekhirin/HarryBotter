00f5944caf89ada05937a7e77fa3b9f74d583776
elishanto <elishanto@gmail.com>
2016-05-04 00:05:29 +0300

    Remove dot from restrict_len()

--- utils/text_formatter.py
+++ utils/text_formatter.py
@@ -1,6 +1,4 @@
 def restrict_len(content, url):
     if len(content) + len(url) > 320:
         content = content[:310 - len(url) - 3].strip() + '...'
-    else:
-        content += '.'
     return content
